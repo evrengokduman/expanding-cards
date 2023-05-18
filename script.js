9fd013869f5f351a73eeeab2ff59db3fe706f22f
evrengokduman <sukruevrengokduman@gmail.com>
2023-05-18 16:46:07 +0300

    Added removing active class function

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const panels = document.querySelectorAll('.panel')
 
 panels.forEach((panel) => {
   panel.addEventListener('click', () => {
+    removeClassActive()
     panel.classList.toggle('active')
   })
 })
+
+function removeClassActive() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active')
+  })
+}
